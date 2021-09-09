f3be9749076d8806e142766b75924a41e3835f06
Luat <you@example.com>
2021-09-09 20:41:16 +0700

    09/09

--- src/components/Home-desktop/About Fetch/AboutFetch.jsx
+++ src/components/Home-desktop/About Fetch/AboutFetch.jsx
@@ -1,34 +1,98 @@
-import React from "react";
+import { Col, Row } from "antd";
+import React, { useEffect, useState } from "react";
 import "../../../assets/css/about.css";
+import AboutTheme from "../../../assets/img/AboutTheme.png";
 function AboutFetch() {
+  const [position, setPosition] = useState(0);
+  const [current, setCurrent] = useState(1);
+
+  const moveLeft = (num) => {
+    let step = num == 0 ? 0 : num ? num : 1;
+    const slide = document.querySelector("#cc");
+    const slideWidth = slide.clientWidth;
+    setPosition(position + step * (slideWidth + 50));
+  };
+  const switchSlide = (num) => {
+    moveLeft(current - num);
+    setCurrent(num);
+  };
+
+  useEffect(() => {
+    const activeDot = document.querySelector("#c" + current);
+    activeDot?.classList.add("active");
+    for (let i = 1; i <= 3; i++) {
+      if (current != i) {
+        document.querySelector("#c" + i).classList.remove("active");
+      }
+    }
+  }, [position, current]);
   return (
-    <div className="about-container">
-      <div className="about__inner">
-        <div className="text-group about__text-group ">
-          <div className="text-group__title about__title ">All about Fetch</div>
-          <div className="text-group__small-text about__small-text ">
-            <span>
-              Separated they live in Bookmarks right at the coast of the famous
-              Semantics,
-              <br /> large language ocean Separated they live in Bookmarks right
-            </span> 
+    <div className="about-container c-cus-container">
+      <div
+        className="about-container__inner"
+        style={{ backgroundImage: `url(${AboutTheme})` }}
+      >
+        <div className="about-container__text-group">
+          <div className="c-h3-title">A leader in tech</div>
+          <div className="c-h6-subtitle" style={{ color: "#A4B7C8" }}>
+            From humble beginnings with just 12 persons in 2014, Fetch is now an
+            established name in the tech industry with an enrolment of over 150
+            employees today.
           </div>
         </div>
-        <div className="about__features">
-          <div className="about__features__inner row">
-            <div className="about__feature col-md-4">
-              <div className="about__count">500+</div>
-              <div className="about__subtitle">Employees</div>
+        <Row
+          className="feature__track"
+          id = "c"
+          style={{ transform: `translateX(${position + "px"})` }}
+        >
+          <Col id="cc" md={6} offset={6} className="about__feature">
+            <div className="feature-num">500+</div>
+            <div className="c-h6-subtitle feature-des">
+              successful deployments
             </div>
-            <div className="about__feature col-md-4">
-              <div className="about__count">100+</div>
-              <div className="about__subtitle">Partners</div>
+          </Col>
+          <Col md={6} offset={6} className="about__feature">
+            <div className="feature-num">7+</div>
+            <div className="c-h6-subtitle feature-des">
+              years in the business
             </div>
-            <div className="about__feature col-md-4">
-              <div className="about__count">7+</div>
-              <div className="about__subtitle">Users</div>
+          </Col>
+          <Col md={6} className="about__feature">
+            <div className="feature-num">USD 5 million</div>
+            <div className="c-h6-subtitle feature-des">remunerated to date</div>
+          </Col>
+          <Col md={6} offset={6} className="about__feature">
+            <div className="feature-num">100+</div>
+            <div className="c-h6-subtitle feature-des">
+              partnerships and growing
             </div>
-          </div>
+          </Col>
+        </Row>
+        <div className="switch-dots">
+          <div
+            onClick={() => switchSlide(1)}
+            className="dot active"
+            role="button"
+            id="c1"
+          ></div>
+          <div
+            onClick={() => switchSlide(2)}
+            className="dot"
+            role="button"
+            id="c2"
+          ></div>
+          <div
+            onClick={() => switchSlide(3)}
+            className="dot"
+            role="button"
+            id="c3"
+          ></div>
+          <div
+            onClick={() => switchSlide(4)}
+            className="dot"
+            role="button"
+            id="c4"
+          ></div>
         </div>
       </div>
     </div>
